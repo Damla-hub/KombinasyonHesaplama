8df368182bb93ff5af195ad1fcc5a4ff697f9eac
Damla Kilinc <dddmlklnc@gmail.com>
2023-09-08 01:04:21 +0300

    first commit

---
+++ src/KombinasyonHesaplama.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class KombinasyonHesaplama {
+    public static void main(String[] args) {
+        int factorialThree=1,factorialOne=1,factorialTwo=1,numberOfElements,subset;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Küme eleman saysıını giriniz : ");
+        numberOfElements=input.nextInt();
+        System.out.print("Alt küme eleman saysını giriniz : ");
+        subset=input.nextInt();
+        int n=numberOfElements-subset;
+        for(int i=1;i<=numberOfElements;i++){
+            factorialOne=factorialOne*i;
+
+        }System.out.println(factorialOne);
+        for(int i =1;i<=subset;i++){
+            factorialTwo*=i;
+        }
+        for (int i=1;i<=n;i++){
+            factorialThree*=i;
+        }
+        double combination = factorialOne/(factorialTwo*factorialThree);
+        System.out.print("C("+numberOfElements+","+subset+")="+combination);
+
+    }
+}
